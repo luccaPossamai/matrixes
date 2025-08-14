22e865627da5660b9adaacd61a466eee72936ec0
lpcamors <luccapmarcos@gmail.com>
2025-08-14 16:31:20 -0300

    updt 14.08.25

---
+++ includes/paths.h
@@ -0,0 +1,9 @@
+#ifndef CONFIGPATHS_H
+#define CONFIGPATHS_H
+
+#include <filesystem>
+
+// declara que existe uma variável COMPILE_PATH em outro cpp
+extern const std::filesystem::path COMPILE_PATH;
+
+#endif
